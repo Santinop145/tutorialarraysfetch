70cd0a97029c94895293ff1a9e9c3466c7d6ecab
Santinop145 <santinoalessandrop@gmail.com>
2024-08-30 10:02:24 -0300

    Añade lo de dragonball a branch dragonball

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-//let products = [
-//    {id: 1, name: 'Plato', price: 3000},
-//    {id: 2, name: 'jordan', price: 2001},
-//    {id: 3, name: 'lebron', price: 1337},
-//    {id: 4, name: '39', price: 777},
-//   {id: 5, name: 'vance', price: 69420},
-//];
-
-const url = 'https://fakestoreapi.com/products';
+const url = 'https://dragonball-api.com/api/characters?limit=58';
+let characterArray = {};
+let randomCardId = 0;
 
 fetch(url)
-    .then(res => res.json())
-    .then(data => { 
-        console.log(data);
-        products = data;
-        let container = document.querySelector('.container');
-        let pre = document.querySelector('#productos');
-        for (const p of products){
-            //pre.textContent += `{ id: ${p.id}, name: ${p.name}, price: ${p.price}}\n`;
-            let productCard = document.createElement('div');
-            let title = document.createElement('h2');
-            let price = document.createElement('p');
-            let image = document.createElement('img');
-            title.textContent = p.title;
-            price.textContent = p.price;
-            image.src = p.image;
-            productCard.append(title);
-            productCard.append(price);
-            productCard.append(image);
-            container.append(productCard);
-        }
-});
+	.then(res => res.json())
+	.then(data => { 
+		let characters = data.items;
+		console.log(data);
+		updateArray(characters);
+	}
+);
+
+function updateArray(characters) {
+	for (let i = 0; i < characters.length; i++) {
+		const character = characters[i];
+		characterArray[i] = character;
+	}
+}
+
+function randomCard(characterArray){
+	let container = document.querySelector('.container');
+	randomCardId = Math.floor(Math.random() * 57);
+	let characterCard = document.createElement('div');
+	let name = document.createElement('h1');
+	let image = document.createElement('img');
+	let ki = document.createElement('h2');
+	image.src = characterArray[randomCardId].image;
+	name.textContent = characterArray[randomCardId].name;
+	ki.textContent = "Ki: " + characterArray[randomCardId].ki;
+	characterCard.append(name);
+	characterCard.append(image);
+	characterCard.append(ki);
+	container.append(characterCard);
+}
